b1e1861929522b5352cb2558e52c26d0257d76fb
GabrielTrettel <gabrielmtrettel@gmail.com>
2020-04-15 15:52:28 -0300

    TutorialActivity for video implemented

--- ModuloConteudo/app/src/main/java/com/ufabc/moduloconteudo/act_tut/TutorialVideoActivity.kt
+++ ModuloConteudo/app/src/main/java/com/ufabc/moduloconteudo/act_tut/TutorialVideoActivity.kt
@@ -1,14 +1,21 @@
 package com.ufabc.moduloconteudo.act_tut
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.MediaController
+import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
 import com.ufabc.moduloconteudo.R
 import com.ufabc.moduloconteudo.act_home.HomeActivity
 
 class TutorialVideoActivity : AppCompatActivity() {
     lateinit var btn_exit : Button
+    lateinit var video_view : VideoView
+    lateinit var videoPath : String
+    lateinit var uri : Uri
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,11 +23,24 @@ class TutorialVideoActivity : AppCompatActivity() {
 
         bindComponents()
         setClickEvents()
+        configureVideoView()
+    }
 
+    private fun configureVideoView() {
+        videoPath = "android.resource://" + packageName + "/" + R.raw.video_example
+        uri = Uri.parse(videoPath)
+        video_view.setVideoURI(uri)
+
+        var media_controller = MediaController(this)
+        video_view.setMediaController(media_controller)
+        media_controller.setAnchorView(video_view)
+
+        video_view.start()
     }
 
     private fun bindComponents() {
         btn_exit = findViewById(R.id.tut_video_btn_exit)
+        video_view = findViewById(R.id.tut_video_videoview)
     }
 
     private fun setClickEvents() {
